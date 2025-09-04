90732b8bc0d5b1b406b0e51e2f176e2f7f081e07
fastslow712 <arthur.heaps>
2025-09-04 14:41:20 -0600

    exprshionsh notes

---
+++ notes/notes_expreshions.py
@@ -0,0 +1,38 @@
+# ah 7th notes
+
+#What is an algorithm?
+#a set of instrucshions to compleat a task
+name=input("what is your name:\n")
+print("hello",name)
+
+#treyson has 12 apples, he has 5 friends he wants to give apples to. howmany apples does each friend get? 2
+apples=12
+friends=5
+print("each friend gets", apples/friends, "apples")
+
+#List steps in an algorithm
+#1.declar vareables (get info) 2.proform any equsions ie do the things 3.display results
+p1=15
+p2=20
+p3=45
+p4=11
+totel_ages=(p1+p2+p3+p4)
+print(totel_ages/4)
+
+#List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+#integer division will remove the desimal
+num1=12
+num2=7
+print("additon(+):",num1+num2)
+print("subtraction(-):",num1*num2)
+print("multiplatshion(*):",num1*num2)
+print("division(/):",num1/num2)
+print("exponents(**):",num1**num2)
+print("integer division(//):",num1//num2)
+print("modulo(%):",num1%num2)
+
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+#
+
+#Why are expressions so important in programming?
+#
