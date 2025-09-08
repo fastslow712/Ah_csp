044923313992b813116228f25ce7369d20852652
fastslow712 <arthur.heaps@ucas-edu.net>
2025-09-08 19:51:53 +0000

    made changes

--- practices/financhal_calculater.py
+++ practices/financhal_calculater.py
@@ -1,11 +1,11 @@
 # ah 7th financhal calculater 
 #rent utilities groceries transport income savinges 10% of income expince/income*100
 
-rent=input("what is your rent\n")
-utilities_cost=input("what is your utilities cost\n")
-groceries_cost=input("what is your groceries cost\n")
-transport_cost=input("what is your transport cost\n")
-income=input("what is your income\n")
+rent=int(input("what is your rent\n"))
+utilities_cost=int(input("what is your utilities cost\n"))
+groceries_cost=int(input("what is your groceries cost\n"))
+transport_cost=int(input("what is your transport cost\n"))
+income=int(input("what is your income\n"))
 
 cost=rent+utilities_cost+groceries_cost+transport_cost
 savings=income*1/10
@@ -13,11 +13,11 @@ rent_percent=rent/income*100
 utilities_percent=utilities_cost/income*100
 groceries_percent=groceries_cost/income*100
 transport_percent=transport_cost/income*100
-spening_money=income-rent-utilities_cost-groceries_cost-transport_cost-savings
+spening_money=income-cost
 
 print(savings)
-print(rent_percent)
-print(utilities_percent)
-print(groceries_percent)
-print(transport_percent)
+print(rent_percent,"% of income")
+print(utilities_percent,"% of income")
+print(groceries_percent,"% of income")
+print(transport_percent,"% of income")
 print(spening_money)
