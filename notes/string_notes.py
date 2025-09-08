9748b07fad0411800a7edd2b59e32c6f9a81df5d
fastslow712 <arthur.heaps>
2025-09-08 14:41:40 -0600

    made notes

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -1,2 +1,35 @@
-#ah 7th notes
-print("I did it!")
+#ah 7th string notes
+
+#What makes something a string?
+#a set of simbles held together by qutashionshioshions 
+#exaples
+first_name=input("what is your first name\n").strip().title()
+last_name=input("what is your last name\n").strip().title()
+sentence='the quick brown fox jumps over the lazy dog.'
+full_name=first_name + " " + last_name
+print("welcome to my program",full_name + "!")
+print(sentence.strip())
+
+#Why do we have strings?
+#
+
+#How do stupid proofing and sanitization relate to each other?
+#
+
+#What is debugging?
+#
+
+#How do you debug the different types of errors?
+#
+
+#Describe what each of the methods below does:
+#find()
+#
+#concatenate (add)
+#
+#upper()
+#
+#lower()
+#
+#strip()
+#
