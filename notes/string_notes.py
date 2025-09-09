ec2dc4a3c43dde8e8274d3b3bdc557f3e137628b
fastslow712 <arthur.heaps>
2025-09-09 14:40:54 -0600

    did notes

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -9,31 +9,43 @@ sentence='the quick brown fox jumps over the lazy dog.'
 full_name=first_name + " " + last_name
 print("welcome to my program",full_name + "!")
 print(sentence.strip())
+print(sentence.find("jumps"))
+print(sentence[20:25])
 
 #Why do we have strings?
 #so we can do things
 
 #How do stupid proofing and sanitization relate to each other?
-#
+#sanitization is part of
 
 #What is debugging?
-#removing bugs
+#fixing code
 
 #How do you debug the different types of errors?
-#
+#you find them and change the code to remove them.
 
 #Describe what each of the methods below does:
 #find()
-#
+#findes a number or word in a vareable
 #concatenate (add)
-#
+#puts to strings together
 #upper()
 #makes all leaters upper case
 #lower()
 #makes all leaters loercase
 #strip()
 #removes extra spaces befor and after a input
+#leangth()
+#find the leangth of a word or veareable
 
 #sanitization and stupid proofing
+# sanitization removing extra spaces corectley captilisies making shure it is the right data type. stupid proofingmaking shure the usre can't break your code
 
 #debuging is fixing code
+#thre types of bugs.
+#sintax erer, gramer mistake. 
+#spaces or tabes in the rong place, forgiting =, or other things, spling error.
+#logic error
+#error that makes the output worng without making the code unable to run.
+#run time error
+#a eror that leats the code work untel the code reaches the error.
