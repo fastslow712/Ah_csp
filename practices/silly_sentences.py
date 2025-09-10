fb5355826b3431567e2d26b999e903108e5a960e
fastslow712 <arthur.heaps>
2025-09-10 14:41:57 -0600

    did silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,6 @@
+#Ah 7th silly_sentences.py
+
+name=input("give me a name\n").strip().title()
+verb=input("give me a verb\n").strip().lower()
+nown=input("give me a nown\n").strip().lower()
+print("Silly" + " " + name + " " + "went to the river to" + " " + verb + " " + nown + " " + "into the river")
