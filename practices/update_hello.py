a2d8481e25ff4769a600cb8833aa8799d8811f58
fastslow712 <arthur.heaps@ucas-edu.net>
2025-09-16 23:24:01 +0000

    updated hello thing.

---
+++ practices/update_hello.py
@@ -0,0 +1,11 @@
+# Ah 7th update hello
+
+def hello(Your_name) :
+    print(f"hi {Your_name}")
+
+name = input("what is your name\n").strip().title()
+hello(name)
+hello("Joe")
+hello("Bob")
+hello("Conrad")
+hello("Rosie")
