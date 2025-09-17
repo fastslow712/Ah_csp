b8cb10c2e30aacde09229bc13212be12b9bb64eb
fastslow712 <arthur.heaps@ucas-edu.net>
2025-09-17 00:22:32 +0000

    updated calculater

---
+++ practices/update_calculator.py
@@ -0,0 +1,10 @@
+# ah 7th update calculator
+
+def monny(type):
+    cost = int(input("what is your, rent\n"))
+    return cost
+monny(cost)
+rent_cost= cost * 1
+print(rent_cost)
+money(utilities_cost)
+utilities_cost="cost"
