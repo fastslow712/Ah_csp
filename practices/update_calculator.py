92cb5edb90f45efa88ba04eb81e5b56aed703800
fastslow712 <arthur.heaps>
2025-09-18 08:55:25 -0600

    did calculater

--- practices/update_calculator.py
+++ practices/update_calculator.py
@@ -1,10 +1,24 @@
 # ah 7th update calculator
 
 def money(type):
-    cost = int(input("what is your", type))
+    cost = int(input("what is your, {type}\n"))
     return cost
 money("rent")
 rent_cost = cost
 
-money(utilities_cost)
+money("utilities cost")
 utilities_cost="cost"
+
+money("groceries cost")
+groceries_cost = cost
+
+money("transport cost")
+transport_cost = cost
+
+money("income")
+income = cost
+
+cost = income + transport_cost + groceries_cost + utilities_cost + rent_cost
+savings=income*1/10
+calculate_persent(type_of_cost)
+    percent=/income*100
