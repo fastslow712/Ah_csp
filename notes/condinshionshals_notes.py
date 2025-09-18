86db2c557168fed214e5ffa0fddc3f8373646ddf
fastslow712 <arthur.heaps>
2025-09-18 14:16:43 -0600

    made notes

--- notes/condinshionshals_notes.py
+++ notes/condinshionshals_notes.py
@@ -1,11 +1,11 @@
 # ah 7th condishonals notes
 
-homework_done = input("is your homework done\n").strip().lower()
+#homework_done = input("is your homework done\n").strip().lower()
 
-if homework_done =="yes":
-    print("yes you can go")
-else:
-    print("then go do your homwork")
+#if homework_done =="yes":
+#    print("yes you can go")
+#else:
+#    print("then go do your homwork")
 
 #What puts something inside of the “if” statement?
 #if it is indented it is in the condishinal
@@ -24,13 +24,16 @@ else:
 grade = 100
 
 if grade >= 90:
-    print(f"you have {grade}% that is an A!")
+    if grade > 100:
+        print(f"you cheated that isn't posible!")
+    else:
+        print(f"you have {grade}% that is an A!")
 elif grade > 80:
     print(f"you have {grade}% that is a B!")
 elif grade >= 70:
     print(f"you have {grade}% that is a c!")
 else:
-    print(f"you have a {grade}%that is a d or lower:(")
+    print(f"you have a {grade}% that is a d or lower:(")
 
 #What do each of the different symbols mean in conditionals?
 
@@ -45,14 +48,21 @@ else:
 #==
 # are they eqwal
 #!
-#not equal to
+#not equal to##
 #What are the 3 logical operators?
-
+#and, or, not.
+name=input("what is your name\n")
+if name == "Ms laRose":
+    print(f"you are the teacher")
+elif name == "Tia" or name =="Ashley":
+    print(f"you are a Ta")
+else:
+    print(f"you are a student")
 
 #What are logical operators for?
-
+#they allow us to combin mutepule condishions
 
 #What does a nested conditional statement do?
-
+#it is a condishnal that is in a condishionl and onley runs if the first condishional is true
 
 #you put the most likley out com in the else
