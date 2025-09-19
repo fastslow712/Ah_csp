4414b44a5e9c5d6cfd2c7a16bcb42e4e1ae817d4
fastslow712 <arthur.heaps>
2025-09-19 16:01:30 -0600

    made calculater work

--- practices/update_calculator.py
+++ practices/update_calculator.py
@@ -1,22 +1,14 @@
 # ah 7th update calculator
 
 def money(type):
-    cost = int(input(f"what is your, {type}\n"))
+    cost = int(input(f"what is your {type}\n"))
     return cost
-money("rent")
-rent_cost = cost
 
-money("utilities cost")
-utilities_cost = cost
-
-money("groceries cost")
-groceries_cost = cost
-
-money("transport cost")
-transport_cost = cost
-
-money("income")
-income = cost
+rent_cost = money("rent")
+utilities_cost = money("utilities cost")
+groceries_cost = money("groceries cost")
+transport_cost = money("transport cost")
+income = money("income")
 
 cost = transport_cost + groceries_cost + utilities_cost + rent_cost
 savings=income*1/10
@@ -24,19 +16,16 @@ def calculate_persent(type_of_cost):
     percent= type_of_cost/income*100
     return percent
 
-calculate_persent(transport_cost)
-transport_percent = percent
-calculate_persent(groceries_cost)
-groceries_percent =percent
-calculate_persent(utilities_cost)
-utilities_percent = percent
-calculate_persent(rent_cost)
-rent_percent = percent
-spening_money = income - transport_cost + groceries_cost + utilities_cost + rent_cost
+transport_percent = calculate_persent(transport_cost)
+groceries_percent = calculate_persent(groceries_cost)
+utilities_percent = calculate_persent(utilities_cost)
+rent_percent = calculate_persent(rent_cost)
+costs = transport_cost + groceries_cost + utilities_cost + rent_cost - savings
+spening_money = income - costs
 
 print(savings,"in savings")
-print(rent_percent,"% of income")
-print(utilities_percent,"% of income")
-print(groceries_percent,"% of income")
-print(transport_percent,"% of income")
-print("your spening_money is", spening_money)
+print(f"rent is: {rent_percent},% of income")
+print(f"utilities is: {utilities_percent},% of income")
+print(f"groceries is: {groceries_percent},% of income")
+print(f"transport is: {transport_percent},% of income")
+print(f"your spening_money is: {spening_money}")
