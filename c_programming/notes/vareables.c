8935526afacf825d79ec5e3545c96b1136f8a5b3
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-02 14:45:30 -0600

    orgnized files

---
+++ c_programming/notes/vareables.c
@@ -0,0 +1,44 @@
+// AH 7Th vareables notes!
+#include <stdio.h>
+
+int main(void){
+    int grade = 95;
+    float pi = 3.14;
+    double ling_pi = 3.1415926358;
+    char letter_grade = 'A';
+    char name[] = "Arthur";
+    printf("%s did it!", name);
+    printf("you have a %d, in the class that is an %c", grade, letter_grade);
+
+    return 0;
+}
+//intigers=int hole numbers 4b floats=float desimal number 4b double=double beater float 8b caricter=char one caricter 1b multipal caricters are a list
+//What is the main difference between declaring variables in Python and C?
+//you hafto spesify the vareable type in c and you end all statments in ;
+//In C, what is the purpose of specifying a data type when declaring a variable?
+//to save space
+//List three common data types used in C and their corresponding format specifiers for printf().
+//
+//How do you declare and initialize an integer variable named "age" with the value 25 in C?
+//
+//What is the difference between printf() and scanf() functions in C?
+//
+//Write a C program that asks the user for their name and age, then prints a greeting message with their information.
+//
+//How do you add comments in C? What are the two types of comments?
+//
+//What is the purpose of the #include <stdio.h> line at the beginning of a C program?
+//
+//In C, what is the significance of the main() function?
+//
+//Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
+//
+//What is the difference between %d and %f format specifiers in printf()?
+//
+//How do you print a newline character in C?
+//
+//Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
+//
+//What is the purpose of the & symbol when using scanf() to get user input?
+//
+//How do you declare a constant in C? Provide an example.
