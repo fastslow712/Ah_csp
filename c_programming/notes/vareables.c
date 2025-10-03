be13f3719ff43a0207014ff6c790cb8a6e0403a3
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-03 14:42:44 -0600

    made notes

--- c_programming/notes/vareables.c
+++ c_programming/notes/vareables.c
@@ -2,43 +2,57 @@
 #include <stdio.h>
 
 int main(void){
-    int grade = 95;
+    int grade;
     float pi = 3.14;
-    double ling_pi = 3.1415926358;
-    char letter_grade = 'A';
+    double long_pi = 3.1415926358;
+    char letter_grade;
     char name[] = "Arthur";
-    printf("%s did it!", name);
-    printf("you have a %d, in the class that is an %c", grade, letter_grade);
+    //user input
+    printf("what is your grade percentige as a whole number: ");
+    scanf("%d", &grade);
+
+    printf("what is your letter grade: ");
+    scanf(" %c", &letter_grade);
+
+    printf("%s did it!\n", name);
+    printf("You have a %d, in the class that is an %c\n", grade, letter_grade);
 
     return 0;
 }
-//intigers=int hole numbers 4b floats=float desimal number 4b double=double beater float 8b caricter=char one caricter 1b multipal caricters are a list
+//intigers=int hole numbers 4bytes %d floats=float desimal number 4butes %f double=double beater float 8bytes %lf caricter=char one caricter 1bytes %c multipal caricters are a list %s
+
 //What is the main difference between declaring variables in Python and C?
 //you hafto spesify the vareable type in c and you end all statments in ;
+
 //In C, what is the purpose of specifying a data type when declaring a variable?
-//to save space
+//to save space 
+
 //List three common data types used in C and their corresponding format specifiers for printf().
-//
+//char vareable_name[] %s char %c int %d
+
 //How do you declare and initialize an integer variable named "age" with the value 25 in C?
-//
+// int age = 25
+
 //What is the difference between printf() and scanf() functions in C?
-//
-//Write a C program that asks the user for their name and age, then prints a greeting message with their information.
-//
-//How do you add comments in C? What are the two types of comments?
-//
+//print is an output scan asines a value to a vareable basted on a user input
+
+//How do you add comments in C? 
+// you use //
+
 //What is the purpose of the #include <stdio.h> line at the beginning of a C program?
-//
+//aliws us to use inputs and outputs.
+
 //In C, what is the significance of the main() function?
-//
-//Write a C program that calculates the area of a rectangle. Ask the user to input the length and width, then display the result.
-//
+//evrething must be inside of a funchion
+
 //What is the difference between %d and %f format specifiers in printf()?
-//
+//d one will not print a number with a desimal f will onley print a number with a desimal
+
 //How do you print a newline character in C?
 //
-//Write a C program that converts temperature from Celsius to Fahrenheit. Ask the user for input in Celsius and display the result in Fahrenheit.
-//
+
 //What is the purpose of the & symbol when using scanf() to get user input?
-//
+//& alows scanf to get the
+
 //How do you declare a constant in C? Provide an example.
+//writ by const it is used to make a vareble unchangable
