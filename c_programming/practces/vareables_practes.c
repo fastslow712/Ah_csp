db3a92907433b8e0f8ee573b7f4c8a7b68b91287
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-06 14:42:14 -0600

    practested vareables

---
+++ c_programming/practces/vareables_practes.c
@@ -0,0 +1,16 @@
+// AH 7Th vareables practes
+//your name a number between 1 and 10 a GPA what you had for breakfast your favorite color the school name the year your eye color your age your favorite subject in school
+
+int main(void){
+    char name[] = Arthur
+    printf("%s", name)
+
+
+
+
+
+
+
+
+
+}
