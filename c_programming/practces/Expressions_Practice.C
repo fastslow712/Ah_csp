c498811deef5fb510a280250ab1889d992bbb215
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-08 22:14:16 +0000

    practested expreshions

--- c_programming/practces/Expressions_Practice.C
+++ c_programming/practces/Expressions_Practice.C
@@ -4,42 +4,42 @@
 
 int main(void){
     int number=7-24/8*4+6;
-    printf("7-24/8*4+6=%d", number);
+    printf("7-24/8*4+6=%d\n", number);
 
     int number_1=18/3-7+2*5;
-    printf("\n18/3-7+2*5=%d", number_1);
+    printf("18/3-7+2*5=%d\n", number_1);
 
     int number_2=6*4/12+72/8-9;
-    printf("\n6*4/12+72/8-9=%d", number_1);
+    printf("6*4/12+72/8-9=%d\n", number_1);
 
     int number_3=17-6/2;
-    printf("\n(17-6/2)+4*3=%d", number_3);
+    printf("(17-6/2)+4*3=%d\n", number_3);
 
     int number_4=1*4-2/2;
     int number_5=6+2-3;
     int number_6=-2*number_4+number_5;
-    printf("\n-2(1*4-2/2)+(6+2-3)= %d", number_6);
+    printf("-2(1*4-2/2)+(6+2-3)=%d\n", number_6);
 
     int number_7=3-4*7;
     int number_8=number_4/5;
     int number_9=-1*number_8-2*24/6;
-    printf("\n-1*[(3-4*7)/5]-2*24/6=%d", number_9);
+    printf("-1*[(3-4*7)/5]-2*24/6=%d\n", number_9);
 
     int number_10=5*5;
     int number_11=3*number_10/15;
     int number_12=5-2*2;
     int number_13=number_11-number_12;
-    printf("\n(3*5^2/15)-(5-2^2)=%d", number_13);
+    printf("(3*5^2/15)-(5-2^2)=%d\n", number_13);
 
     int number_14=3*3*3;
     int number_15=1*2*2+number_14;
     int number_16=2*2*2*2*2;
     int number_17=number_15-number_16/4;
-    printf("\n(1^4*2^2+3^3)-2^5/4=%d", number_17);
+    printf("(1^4*2^2+3^3)-2^5/4=%d\n", number_17);
 
     int number_18=22/2-2*5;
     int number_19=4-6/6;
     int number_20=number_18*number_18+number_19*number_19;
-    printf("\n(22/2-2*5)^2+(4-6/6)^2=", number_12*number_12+number_13*number_13);
+    printf("(22/2-2*5)^2+(4-6/6)^2=%d\n", number_20);
     return 0;
 }
