9ea891dd312f98d3c1ae1a1ab535adc4d9426af3
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-09 00:29:58 +0000

    did financhal calculter

---
+++ c_programming/practces/financhal_calulater.c
@@ -0,0 +1,41 @@
+// ah 7th financhal calculater 
+//rent utilities groceries transport income savinges 10% of income expince/income*100
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    int rent_cost;
+    int utilities_cost;
+    int groceries_cost;
+    int transport_cost;
+    int income;
+
+    printf("what is your rent cost\n");
+    scanf("%d", &rent_cost);;
+    printf("what is your utilities cost\n");
+    scanf(" %d", &utilities_cost);;
+    printf("what is your groceries_cost\n");
+    scanf(" %d", &groceries_cost);;
+    printf("what is your transport_cost\n");
+    scanf(" %d", &transport_cost);;
+    printf("what is your income\n");
+    scanf(" %d", &income);
+
+    int cost=rent_cost+utilities_cost+groceries_cost+transport_cost;
+    int savings=income*1/10;
+    int rent_percent=rent_cost/income*100;
+    int utilities_percent=utilities_cost/income*100 ;
+    int groceries_percent=groceries_cost/income*100;
+    int transport_percent=transport_cost/income*100;
+    int spening_money=income-cost-savings;
+
+    printf("%d$ in savings\n", savings);
+    printf("your rent of %d$ is %d percent of income\n", rent_cost, rent_percent);
+    printf("your utilities of %d$ percent is %d of income\n", utilities_cost, utilities_percent);
+    printf("your groceries of %d$ percent is %d of income\n", groceries_cost, groceries_percent);
+    printf("your transport of %d$ percent is %d of income\n", transport_cost, transport_percent);
+    printf("you have %d$ left to spend\n", spening_money);
+
+    return 0;
+}
