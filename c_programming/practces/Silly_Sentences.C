6f746d261599771b1b11637d8da80cac6b7117a1
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-11 03:19:20 +0000

    made silly sentences

--- c_programming/practces/Silly_Sentences.C
+++ c_programming/practces/Silly_Sentences.C
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 int main(void){
-    char name[50];
-    char verb[50];
-    char plural_nown[50];
+    char name[25];
+    char verb[25];
+    char plural_noun[25];
     printf("give me a name: ");
-    scanf("%s", &name);
+    scanf("%s", name);
     printf("give me a verb: ");
-    scanf(" %s", &verb);
+    scanf(" %s", verb);
     printf("give me a plural nown: ");
-    scanf(" %s", &plural_nown);
-    printf("Silly %s went to the river to %s %s into the river\n", name, verb, plural_nown);
+    scanf(" %s", plural_noun);
+    printf("Silly %s went to the river to %s %s into the river\n", name, verb, plural_noun);
 
     return 0;
 }
