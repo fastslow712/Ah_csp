150df58a7909fce6800c43d970588f23dedb39fd
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-12 22:08:22 +0000

    made old enough test

---
+++ c_programming/practces/old_enough.c
@@ -0,0 +1,20 @@
+//Ah 7th old enough
+#include <stdio.h>
+
+int main(void){ 
+    float age;
+    printf("give me your age: \n");
+    scanf("%f", age);
+    if (age >= 18){
+        printf("you can vote!\n");
+    }else if (age >= 16){
+        printf("you can drive!\n");
+    }else if (age >= 15){
+        printf("you can get a learners permit!\n");
+    }else if (age >= 4){
+        printf("you can go to school!\n");
+    }else{
+        printf("you are not old enough!\n");
+    }
+    return 0;
+}
