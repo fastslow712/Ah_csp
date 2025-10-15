a3c1cc02a154e68af60ea6aa129bc286716d0769
fastslow712 <arthur.heaps@ucas-edu.net>
2025-10-15 05:14:03 +0000

    updated code

--- c_programming/practces/update_calculator.c
+++ c_programming/practces/update_calculator.c
@@ -1,7 +1,37 @@
 // ah 7th update calculator
 #include <stdio.h>
+#include <math.h>
 
-int find_cost(cost_type){
-    printf("what is your %s cost per month\n", cost_type);
-    scanf("%d", &);
+int find_amownt(char* cost_type){
+    int amownt;
+    printf("what is your %s per month\n", cost_type);
+    scanf("%d", &amownt);
+    return amownt;
+}
+int find_percent(int cost, float divider){
+    int percent;
+    percent=cost/divider*100;
+}
+void print_results(char* type, int cost, int percent){
+    printf("your %s of %d$ is %d percent of income\n", type, cost, percent);
+}
+int main(void){
+    int rent_cost = find_amownt("rent");
+    int utilities_cost = find_amownt("utilities");
+    int groceries_cost= find_amownt("groceries");
+    int transport_cost= find_amownt("transport");
+    float income = find_amownt("income");
+    int cost=rent_cost+utilities_cost+groceries_cost+transport_cost;
+    int savings=income*1/10;
+    int rent_percent = find_percent(rent_cost, income);
+    int utilities_percent = find_percent(utilities_cost, income);
+    int groceries_percent = find_percent(groceries_cost, income);
+    int transport_percent = find_percent(transport_cost, income);
+    printf("%d$ in savings\n", savings);
+    print_results("rent", rent_cost, rent_percent);
+    print_results("utilities", utilities_cost, utilities_percent);
+    print_results("groceries", groceries_cost, groceries_percent);
+    print_results("transport", transport_cost, transport_percent);
+
+    return 0;
 }
--- c_programming/practces/update_hello.c
+++ c_programming/practces/update_hello.c
@@ -1,27 +1,27 @@
 // ah 7th update hello
 #include <stdio.h>
 
-void print_name(char name){
+void print_name(char** name){
     printf("Hi %s\n", name);
+}
+char** enter_name(void){
+    char** name[10];
+    printf("give me a name\n");
+    scanf("%s", &name);
+    return name;
 }
 
 int main(void){
-    char* argument[10];
-    printf("give me a name\n");
-    scanf("%s", &argument);
-    print_name(argument);
-    printf("give me a name\n");
-    scanf("%s", &argument);
-    print_name(argument);
-    printf("give me a name\n");
-    scanf("%s", &argument);
-    print_name(argument);
-    printf("give me a name\n");
-    scanf("%s", &argument);
-    print_name(argument);
-    printf("give me a name\n");
-    scanf("%s", &argument);
-    print_name(argument);
+    char** name[] = enter_name;
+    print_name(name);
+    char** name[] = enter_name;
+    print_name(name);
+    char** name[] = enter_name;
+    print_name(name);
+    char** name[] = enter_name;
+    print_name(name);
+    char** name[] = enter_name;
+    print_name(name);
 
     return 0;
 }
